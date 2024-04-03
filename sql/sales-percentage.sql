fbda710678ef2928212aa9339d2c4f4364684652
Ignacio Carazo <ignaciocarazo@Ignacios-MacBook-Pro-2.local>
2024-04-02 21:26:36 -0600

    add sql code

---
+++ sql/sales-percentage.sql
@@ -0,0 +1,24 @@
+WITH ProductSales AS (
+    SELECT 
+        product_id,
+        SUM(unit_price * quantity_of_items_sold) AS total_sales
+    FROM 
+        sales
+    GROUP BY 
+        product_id
+),
+TotalSales AS (
+    SELECT 
+        SUM(unit_price * quantity_of_items_sold) AS overall_total_sales
+    FROM 
+        sales
+)
+SELECT 
+    ps.product_id,
+    ps.total_sales,
+    round((ps.total_sales / ts.overall_total_sales) * 100,2) AS percentage_of_total_sales
+FROM 
+    ProductSales ps
+CROSS JOIN 
+    TotalSales ts
+ORDER BY percentage_of_total_sales DESC;
