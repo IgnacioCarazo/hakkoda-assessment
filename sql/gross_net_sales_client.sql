ae4b218a41774c7615db2adee1e91fa24785e00e
Ignacio Carazo <ignaciocarazo@Ignacios-MacBook-Pro-2.local>
2024-04-02 22:21:06 -0600

    rename and add more metrics

--- sql/gross_net_sales_client.sql
+++ sql/gross_net_sales_client.sql
@@ -1,13 +1,13 @@
-CREATE TABLE gross_net_sales AS
-SELECT product_id,
+CREATE TABLE gross_net_sales_client AS
+SELECT client_id,
        ROUND(gross_sale, 2) AS gross_sale,
-       net_sale,
+       Round(net_sale, 2),
        ROUND(gross_sale - net_sale, 2) AS lost_to_discount
 FROM (
-    SELECT product_id, 
+    SELECT client_id, 
            SUM(unit_price * quantity_of_items_sold) AS gross_sale, 
            SUM((1 - discount) * unit_price * quantity_of_items_sold) AS net_sale
     FROM sales
-    GROUP BY product_id
+    GROUP BY client_id
 ) AS subquery
-ORDER BY gross_sale DESC;
+ORDER BY client_id ASC;
---
+++ sql/gross_net_sales_product.sql
@@ -0,0 +1,13 @@
+CREATE TABLE gross_net_sales_product AS
+SELECT product_id,
+       ROUND(gross_sale, 2) AS gross_sale,
+       net_sale,
+       ROUND(gross_sale - net_sale, 2) AS lost_to_discount
+FROM (
+    SELECT product_id, 
+           SUM(unit_price * quantity_of_items_sold) AS gross_sale, 
+           SUM((1 - discount) * unit_price * quantity_of_items_sold) AS net_sale
+    FROM sales
+    GROUP BY product_id
+) AS subquery
+ORDER BY gross_sale DESC;
